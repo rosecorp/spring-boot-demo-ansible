6671e6b57f8b158c956e57d8408fd550e683f342
jk <dakoma@gmail.com>
2017-09-22 13:42:43 +0100

    jk - application property read from external soruce.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # spring-boot-demo-ansible
+
+To run an app and override `default.properties`
+
+run jar as following: `java -jar ansible-demo-spring-boot-0.0.1-SNAPSHOT.jar --spring.config.location=app.properties`
+where `spring.config.location` is a location for property file
--- src/main/java/com/ansible/spring/boot/ansibledemospringboot/AnsibleDemoSpringBootApplication.java
+++ src/main/java/com/ansible/spring/boot/ansibledemospringboot/AnsibleDemoSpringBootApplication.java
@@ -1,20 +1,29 @@
 package com.ansible.spring.boot.ansibledemospringboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@PropertySource(value = {
+        "classpath:default.properties",
+        "${external.properties}"},
+        ignoreResourceNotFound = true)
 @RestController
 public class AnsibleDemoSpringBootApplication {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Star Wars!";
-	}
+    @Value("${movie.name}")
+    private String movieName;
 
-	public static void main(String[] args) {
-		SpringApplication.run(AnsibleDemoSpringBootApplication.class, args);
-	}
+    @RequestMapping("/")
+    public String index() {
+        return movieName;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnsibleDemoSpringBootApplication.class, args);
+    }
 }
